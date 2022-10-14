cf4203b807affe157dfc6d81745d0abad9b7ed14
amityadav545 <113887921+amityadav545@users.noreply.github.com>
2022-10-14 17:36:53 +0530

    update

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../List/list.scss'
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Navbar from '../../Components/Navbar/Navbar'
-import DataTable from '../../Components/DataTable/DataTable'
+import DataTable from '../../Components/DataTable/DataTable';
+import PersonIcon from '@mui/icons-material/Person';
 export default function List() {
     return (
         <div className='list'>
@@ -10,10 +13,15 @@ export default function List() {
             <div className="listcontainer">
                 <Navbar />
                 <div className='sectiondatatable' style={{ width: '100%', height: '100%' }}>
-                    <div><span className='nametitle'>User Table</span></div>
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div>
+                            <span className='nametitle'>User Table</span>
+                        </div>
+                        <Link to='/user/new' style={{ textDecoration: 'none', marginRight: '40px', marginTop: '10px' }}> <Button variant="outlined">Add New <PersonIcon /></Button></Link>
+                    </div>
                     <DataTable />
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
--- src/Pages/New/New.jsx
+++ src/Pages/New/New.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../New/new.scss'
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Navbar from '../../Components/Navbar/Navbar'
+import Button from '@mui/material/Button';
 export default function New() {
     return (
         <div className='new'>
@@ -17,7 +19,48 @@ export default function New() {
                             <img className='newimages' src='https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg' alt='' />
                         </div>
                     </div>
-                    <div className='right'>right</div>
+                    <div className='right'>
+                        <form>
+                            <div className="formInput">
+                                <label htmlFor='image'> <AddPhotoAlternateIcon className='iconimg' /></label>
+                                <input type='file' id='image' style={{ display: 'none' }} />
+
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='username'>User Name</label>
+                                <input type='text' placeholder='@amit63' id='username' />
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='Fullname'>Full Name</label>
+                                <input type='text' placeholder='enter full name' id='Fullname' />
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='mobile'>Mobile</label>
+                                <input type='number' placeholder='enter mobile number' id='mobile' />
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='email'>Email</label>
+                                <input type='email' placeholder='enter your email' id='email' />
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='age'>Age</label>
+                                <input type='number' placeholder='Enter Age' id='age' />
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='address'>Address</label>
+                                <input type='text' placeholder='Enter Address' id='address' />
+                            </div>
+                            <div className="formInput">
+                                <label htmlFor='password'>Password</label>
+                                <input type='text' placeholder='Enter Address' id='password' />
+                            </div>
+
+
+
+                        </form>
+
+                        <Button className='buttomform' variant="contained">Send</Button>
+                    </div>
                 </div>
             </div>
         </div>
